70525dfec8189abb89a179818534b8b068d44b6c
Jacopo2125 <jacopovedovato1225@gmail.com>
2025-02-15 14:59:34 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, jsonify
+import json
+
+app = Flask(__name__)
+
+# Funzione per caricare il JSON
+def load_summary():
+    with open("data/summary.json", "r", encoding="utf-8") as file:
+        return json.load(file)
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+# API per ottenere i dati dal JSON
+@app.route("/api/summary")
+def api_file():
+    data = load_summary()
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
